f7242654dadf59f960ab797df0402028e26ef65e
Chirag <khoalachirag26@gmail.com>
2025-08-24 19:05:44 +0530

    shortening urls and built all endpoints

--- url-shortener-sb/src/main/java/com/url/url_shortener_sb/security/WebSecurityConfig.java
+++ url-shortener-sb/src/main/java/com/url/url_shortener_sb/security/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(auth-> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/urls/**").authenticated()
-                        .requestMatchers("/{shortUrl").permitAll()
+                        .requestMatchers("/{shortUrl}").permitAll()
                         .anyRequest().authenticated()
                 );
         http.authenticationProvider(authenticationProvider()) ;
