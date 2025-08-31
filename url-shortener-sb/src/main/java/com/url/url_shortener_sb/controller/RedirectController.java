e8cc11120543b2ae34f5255b449663007569eeb4
Chirag <khoalachirag26@gmail.com>
2025-08-31 16:20:47 +0530

    redirect functionality apis

--- url-shortener-sb/src/main/java/com/url/url_shortener_sb/controller/RedirectController.java
+++ url-shortener-sb/src/main/java/com/url/url_shortener_sb/controller/RedirectController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-public class RedirectController {
+public class  RedirectController {
 
     private UrlMappingService urlMappingservice;
 
